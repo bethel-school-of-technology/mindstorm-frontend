419c463282ace3cb569fcde3df03be7ad75daada
tavocortez <ocortez007@gmail.com>
2019-05-11 13:44:24 -0700

    added characterPost Methods and characterService methods for backend

--- src/app/components/character-post/character-post.component.ts
+++ src/app/components/character-post/character-post.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap} from '@angular/router';
+import { CharacterService } from '../../shared/character.service';
+import { Character } from '../../shared/character.model';
+
 
 @Component({
   selector: 'app-character-post',
@@ -6,10 +11,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-post.component.css']
 })
 export class CharacterPostComponent implements OnInit {
- 
-  constructor() { }
+  characterTitle = '';
+  characterDetail = '';
+  character: Character;
+  private mode = 'create';
+  private characterId: string;
+
+  constructor(public characterService: CharacterService, public route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('characterId')) {
+        this.mode = 'edit';
+        this.characterId = paramMap.get('characterId');
+        this.characterService.getCharacter(this.characterId).subscribe(characterData => {
+          this.character = {id: characterData._id, title: characterData.title, detail: characterData.detail};
+        });
+      } else { this.mode = 'create'; this.characterId = null; }
+    });
+  }
+
+  onSaveCharacter(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    if (this.mode === 'create') {
+      this.characterService.addCharacter(form.value.title, form.value.detail);
+    } else {
+      this.characterService.updateCharacter(this.characterId, form.value.title, form.value.detail);
+    }
+    form.resetForm();
   }
 
 }
--- src/app/shared/character.service.ts
+++ src/app/shared/character.service.ts
@@ -4,7 +4,7 @@ import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { Character } from './character.model';
-import { stringify } from '@angular/compiler/src/util';
+
 
 
 @Injectable({
@@ -27,12 +27,24 @@ export class CharacterService {
   }
 
   addCharacter(title: string, detail: string) {
-    const character: Character = {id: null, title: title, detail: detail};
+    const character: Character = {id: null, title, detail};
     this.http.post<{message: string; characterId: string}>('http://localhost:3000/api/characters', character)
     .subscribe(responseData => {
       const id = responseData.characterId;
       character.id = id;
       this.characters.push(character);
+      this.charactersUpdated.next([...this.characters]);
+      this.router.navigate(['/']);
+    });
+  }
+
+  updateCharacter(id: string, title: string, detail: string) {
+    const character: Character = {id, title, detail};
+    this.http.put('http://localhost:3000/api/characters/' + id, character)
+    .subscribe(response => {
+      const updatedCharacters = [...this.characters];
+      const oldCharacterIndex = updatedCharacters.findIndex(c => c.id === character.id);
+      updatedCharacters[oldCharacterIndex] = character;
       this.charactersUpdated.next([...this.characters]);
       this.router.navigate(['/']);
     });
