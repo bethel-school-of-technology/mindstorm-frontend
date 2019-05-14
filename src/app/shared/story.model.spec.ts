98191531bdc62465c38ff52a311b2e07e8c4f1a6
tavocortez <ocortez007@gmail.com>
2019-05-13 17:27:04 -0700

    added storypost Component, service and model and properties for model; also set up a route for stroy Post

--- src/app/shared/story.model.spec.ts
+++ src/app/shared/story.model.spec.ts
@@ -1,7 +1,7 @@
-import { Story.Model } from './story.model';
+import { Story } from './story.model';
 
 describe('Story.Model', () => {
   it('should create an instance', () => {
-    expect(new Story.Model()).toBeTruthy();
+    expect(new Story()).toBeTruthy();
   });
 });
--- src/app/shared/story.service.ts
+++ src/app/shared/story.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Character } from './character.model';
 
 @Injectable({
   providedIn: 'root'
