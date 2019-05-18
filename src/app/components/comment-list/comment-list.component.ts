26667298d2bd0b43a6a8248929b00b51ef74bb80
tavocortez <ocortez007@gmail.com>
2019-05-18 13:25:07 -0700

    commentlist component and service methods done

--- src/app/components/comment-list/comment-list.component.ts
+++ src/app/components/comment-list/comment-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Comment } from '../../shared/comment.model';
+import { CommentService } from '../../shared/comment.service';
 
 @Component({
   selector: 'app-comment-list',
   templateUrl: './comment-list.component.html',
   styleUrls: ['./comment-list.component.css']
 })
-export class CommentListComponent implements OnInit {
+export class CommentListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  comments: Comment[] = [];
+  private commentSub: Subscription;
+
+  constructor(public commentService: CommentService) { }
 
   ngOnInit() {
+    this.commentService.getComments();
+    this.commentSub = this.commentService.getCommentUpdateListener()
+    .subscribe((comments: Comment[]) => {
+      this.comments = comments;
+    });
   }
-
+  onDelete(commentId: string) {
+    this.commentService.deleteComment(commentId);
+  }
+  ngOnDestroy() {
+    this.commentSub.unsubscribe();
+  }
 }
--- src/app/shared/comment.service.ts
+++ src/app/shared/comment.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Comment } from './comment.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
+  private comments: Comment[] = [];
+  private commentsUpdated = new Subject<Comment[]>();
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+// This will grab comments from database
+  getComments() {
+    this.http.get<{ message: string; comments: any }>('http://localhost:3000/api/comments')
+    .pipe(map(commentData => {
+      return commentData.comments.map(comment => {
+        return {
+          postTitle: comment.postTitle,
+          postBody: comment.postBody,
+          id: comment._id
+        };
+      });
+    }))
+    .subscribe(formedComments => {
+      this.comments = formedComments;
+      this.commentsUpdated.next([...this.comments]);
+    });
+  }
+// This will get Comments and return them to be seen on webpage
+  getCommentUpdateListener() {
+    return this.commentsUpdated.asObservable();
+  }
+// This will delete comments from the webpage and database
+  deleteComment(commentId: string)  {
+    this.http.delete('http://localhost:3000/api/comments' + commentId)
+    .subscribe(() => {
+      const updatedComments = this.comments.filter(comment => comment.id !== commentId);
+      this.comments = updatedComments;
+      this.commentsUpdated.next([...this.comments]);
+    });
+  }
+
 }
