5f683e5b104b92de4272e5a0eefabc2de4aeaa9d
msiksnis <msiksnis@gmail.com>
2019-05-30 17:01:30 +0200

    Fixed user to stay loged in after page is reloaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { AuthServiceService } from "./components/user/auth-service.service";
 
 /**
  * The main component.
  */
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthServiceService) {}
+
+  ngOnInit() {
+    this.authService.autoAuthUser();
+  }
   /**
    * title is referred to as frontend.
    */
-  title = 'frontend';
+  title = "frontend";
 }
